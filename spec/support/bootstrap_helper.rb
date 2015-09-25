cd5ba910940fcf1ba8452579b3dca6f1af0220a7
Ali Tayarani <ali.tayarani@tapjoy.com>
2015-09-25 15:16:03 -0400

    fixed elb specs

--- spec/support/bootstrap_helper.rb
+++ spec/support/bootstrap_helper.rb
@@ -28,13 +28,44 @@ module BootstrapHelper
     let(:new_config)              { environment.first }
     let(:aws_env)                 { environment[1] }
     let(:user_data)               { util.generate_user_data(config_dir:opts[:config_dir], **new_config) }
-    let(:groups)                  { util.get_security_groups(opts[:env], new_config[:group]) }
+    let(:groups)                  { util.get_security_groups(opts[:config_dir], opts[:env], new_config[:group]) }
+    # let(:elb_hash)                {{
+    #   elb_name => new_config[:default_elb_parameters].merge!(elb_port: 80)
+    # }}
+    let(:zones)                    { new_config[:zones] }
+    let(:security_groups)          { groups }
+
+    before(:all) do
+      @util = Tapjoy::AutoscalingBootstrap::Base.new
+      @elb_name = 'test-elb'
+      @cluster_filename = 'test_cluster.yaml'
+      @opts = {
+        env:        'qa',
+        filename:   @cluster_filename,
+        config_dir: 'spec/fixtures'
+      }
+      # # defaults_hash = util.load_yaml("#{opts[:config_dir]}/common/defaults.yaml")
+      # config = util.load_yaml(cluster_filename, 'qa', opts[:config_dir])
+      # # common_hash = util.load_yaml("#{opts[:config_dir]}/common/qa.yaml")
+      @environment = @util.configure_environment(
+        @opts[:filename],
+        @opts[:env],
+        @opts[:config_dir]
+      )
+      @elb_hash = {
+        @elb_name => @environment.first[:default_elb_parameters].merge!(elb_port: 80)
+      }
+      @clobber_elb = true
+      @zones = @environment.first[:zones]
+      @security_groups = @util.get_security_groups(
+        @opts[:config_dir], @opts[:env], @environment.first[:group])
+    end
 
     before(:each) do
       Aws.config[:region] = new_config[:aws_region]
       Tapjoy::AutoscalingBootstrap.scaler_name = "#{cluster_name}-group"
       Tapjoy::AutoscalingBootstrap.config_name = "#{cluster_name}-config"
-      Tapjoy::AutoscalingBootstrap.elb_name = elb_name
+      Tapjoy::AutoscalingBootstrap.elb_name = @elb_name
     end
   end
 end
--- spec/tapjoy/autoscaling_bootstrap/elb_spec.rb
+++ spec/tapjoy/autoscaling_bootstrap/elb_spec.rb
@@ -2,62 +2,43 @@ require 'spec_helper'
 
 describe Tapjoy::AutoscalingBootstrap::ELB, :vcr do
   describe '#new' do
-    let(:config) { new_config.merge({
-      elb_health_target: 'http:80/healthz',
-      elb_protocol:      'http',
-      instance_port: 80,
-      elb_port: 80,
-    })}
     it 'name too long', :elb => 'name_error' do
       Tapjoy::AutoscalingBootstrap.elb_name = 'This name is far too long for an elastic load balancer'
-      expect{Tapjoy::AutoscalingBootstrap::ELB.new.create(config,
-        aws_env)}.to raise_error {Tapjoy::AutoscalingBootstrap::Errors::ELB::NameTooLong}
+      expect{
+        Tapjoy::AutoscalingBootstrap::ELB.new(
+          @elb_hash, @clobber_elb, @zones, @security_groups
+        )
+      }.to raise_error {Tapjoy::AutoscalingBootstrap::Errors::ELB::NameTooLong}
     end
   end
 
   context 'creates an elb', :elb => 'create' do
+    before(:all) do
+      Tapjoy::AutoscalingBootstrap.elb_name = @elb_name
+      @elb = Tapjoy::AutoscalingBootstrap::ELB.new(
+        @elb_hash, @clobber_elb, @zones, @security_groups)
+    end
+
     describe '#create' do
-      let(:config) { new_config.merge({
-        elb_health_target: 'http:80/healthz',
-        elb_protocol:      'http',
-        instance_port: 80,
-        elb_port: 80,
-        elb_name: 'test-elb'
-      })}
-
-      # let(:elb_name) {'test-elb'}
-
       it 'has valid params' do
         puts 'Creating ELB...'
-        expect{Tapjoy::AutoscalingBootstrap::ELB.new.create(config,
-          aws_env)}.to_not raise_error
+        expect{@elb.create(
+          @elb_hash[Tapjoy::AutoscalingBootstrap.elb_name])}.to_not raise_error
       end
     end
 
     describe '#health_check' do
-      let(:config) { new_config.merge({
-        elb_health_target: 'http:80/healthz',
-        elb_protocol:      'http',
-        instance_port: 80,
-        elb_port: 80,
-        elb_health_interval: 15,
-        elb_health_timeout: 5,
-        elb_unhealthy_threshold: 2,
-        elb_healthy_threshold: 2
-      })}
       it 'has valid params' do
         puts 'Configuring health check...'
-
-        elb = Tapjoy::AutoscalingBootstrap::ELB.new
-        expect(elb).to be_instance_of Tapjoy::AutoscalingBootstrap::ELB
-        expect{elb.health_check(config)}.to_not raise_error
+        expect(@elb).to be_instance_of Tapjoy::AutoscalingBootstrap::ELB
+        expect{@elb.health_check(
+          @elb_hash[Tapjoy::AutoscalingBootstrap.elb_name])}.to_not raise_error
       end
     end
 
-    after :all do
+    after(:all) do
       puts 'Cleaning up...'
-      elb = Tapjoy::AutoscalingBootstrap::ELB.new
-      elb.delete if elb.exists
+      @elb.delete if @elb.exists
     end
   end
 end
