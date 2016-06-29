73ddaba8792dbb76f3401716025f04def5ab0fc9
Ali Tayarani <ali.tayarani@tapjoy.com>
2016-06-29 12:50:52 -0400

    add enable_metrics

--- lib/tapjoy/autoscaling_bootstrap/version.rb
+++ lib/tapjoy/autoscaling_bootstrap/version.rb
@@ -3,7 +3,7 @@ module Tapjoy
     module Version
       MAJOR = 1
       MINOR = 0
-      PATCH = 3
+      PATCH = 4
     end
 
     VERSION = [Version::MAJOR, Version::MINOR, Version::PATCH].join('.')
